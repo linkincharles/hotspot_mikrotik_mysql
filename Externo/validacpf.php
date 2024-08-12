ceb16041798ba3a6ab94c9305b612c3661da63e0
linkincharles <linkincharles@gmail.com>
2024-08-12 18:21:41 -0300

    att
    att

--- Externo/validacpf.php
+++ Externo/validacpf.php
@@ -2,42 +2,38 @@
 function validaCPF($cpf = null) {
 
     // Verifica se um número foi informado
-    if(empty($cpf)) {
+    if (empty($cpf)) {
         return false;
     }
 
-    // Elimina possivel mascara
+    // Elimina possível máscara
     $cpf = preg_replace("/[^0-9]/", "", $cpf);
     $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
     
-    // Verifica se o numero de digitos informados é igual a 11 
+    // Verifica se o número de dígitos informados é igual a 11
     if (strlen($cpf) != 11) {
         return false;
     }
-    // Verifica se nenhuma das sequências invalidas abaixo 
-    // foi digitada. Caso afirmativo, retorna falso
+    // Verifica se nenhuma das sequências inválidas foi digitada
     else if ($cpf == '00000000000' || 
-        $cpf == '11111111111' || 
-        $cpf == '22222222222' || 
-        $cpf == '33333333333' || 
-        $cpf == '44444444444' || 
-        $cpf == '55555555555' || 
-        $cpf == '66666666666' || 
-        $cpf == '77777777777' || 
-        $cpf == '88888888888' || 
-        $cpf == '99999999999') {
+             $cpf == '11111111111' || 
+             $cpf == '22222222222' || 
+             $cpf == '33333333333' || 
+             $cpf == '44444444444' || 
+             $cpf == '55555555555' || 
+             $cpf == '66666666666' || 
+             $cpf == '77777777777' || 
+             $cpf == '88888888888' || 
+             $cpf == '99999999999') {
         return false;
-     // Calcula os digitos verificadores para verificar se o
-     // CPF é válido
-     } else {   
-        
+    } else {   
+        // Calcula os dígitos verificadores para verificar se o CPF é válido
         for ($t = 9; $t < 11; $t++) {
-            
             for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf{$c} * (($t + 1) - $c);
+                $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($cpf{$c} != $d) {
+            if ($cpf[$t] != $d) {
                 return false;
             }
         }
@@ -45,5 +41,4 @@ function validaCPF($cpf = null) {
         return true;
     }
 }
-
 ?>
